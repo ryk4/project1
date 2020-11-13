5c9243878b54613f9f0ea3f7dc454bc713eaa884
unknown <rytiskli@gmail.com>
2020-11-13 08:58:46 +0200

    imported theme

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -78,7 +78,7 @@
 
                                     </div>
 
-                                <
+
                             @endforeach
                         @endisset
                     </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -34,7 +34,7 @@
                     {{ config('app.name', 'Recipe app') }}
                 </a>
 
-                <a href="{{url('recipe')}}">Add recipe</a>
+                <a href="{{url('recipe')}}">New home</a>
 
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
